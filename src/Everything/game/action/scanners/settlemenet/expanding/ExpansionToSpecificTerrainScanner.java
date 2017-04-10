7abfc611fee3560c65f8dbadbcccc124cf7e1aee
Cesar Atteh <cesar.atteh@gmail.com>
2017-04-10 02:07:52 -0400

    fixed ExpansionToSpecificTerrainScanner (and renamed it to that name)

--- src/Everything/game/action/scanners/settlemenet/expanding/ExpansionToSpecificTerrainScanner.java
+++ src/Everything/game/action/scanners/settlemenet/expanding/ExpansionToSpecificTerrainScanner.java
@@ -12,18 +12,18 @@ import java.util.ArrayList;
  * Takes in a Settlement, map, and terrain type
  * returns an arraylist of mapspots that can be expanded to from a the given settlement for the givern terrain type
  */
-public class ExpansionScannerTerrain {
+public class ExpansionToSpecificTerrainScanner {
 
     //---------------
     // Public Methods
 
-    public ArrayList<MapSpot> scan(final Settlement settlement, final Map map, Terrain terraintype) throws NoValidActionException {
+    public ArrayList<MapSpot> scan(final Settlement settlement, final Map map, Terrain expansionTerrain) throws NoValidActionException {
 
         final ArrayList<MapSpot> expandableMapSpots = new ArrayList<>();
 
         final boolean visited[][][] = new boolean[Map.size()][Map.size()][Map.size()];
 
-        visit(settlement.getMapSpots().get(0), expandableMapSpots, visited, map, settlement, terraintype);
+        visit(settlement.getMapSpots().get(0), expandableMapSpots, visited, map, settlement, expansionTerrain);
 
         if(expandableMapSpots.size() < 1)
             throw new NoValidActionException("No available expansion spots");
@@ -34,7 +34,12 @@ public class ExpansionScannerTerrain {
     //----------------
     // Private Methods
 
-    private void visit(final MapSpot currentMapSpot, final ArrayList<MapSpot> expandableMapSpots, boolean visited[][][], final Map map, final Settlement settlement, final Terrain terraintype) {
+    private void visit(final MapSpot currentMapSpot,
+                       final ArrayList<MapSpot> expandableMapSpots,
+                       boolean visited[][][],
+                       final Map map,
+                       final Settlement settlement,
+                       final Terrain terraintype) {
 
         if(visited[currentMapSpot.getX()][currentMapSpot.getY()][currentMapSpot.getZ()])
             return;
@@ -54,12 +59,13 @@ public class ExpansionScannerTerrain {
 
     private boolean satisfiesExpansionRequirements(final MapSpot mapSpot, final Map map, final Settlement settlement, Terrain terraintype) {
         return satisfiesVisitingRequirements(mapSpot, map, settlement, terraintype) &&
-                map.getHexagon(mapSpot).isEmpty();
+                map.getHexagon(mapSpot).isEmpty() &&
+                map.getHexagon(mapSpot).getTerrainType() == terraintype;
     }
 
     private boolean satisfiesVisitingRequirements(final MapSpot mapSpot, final Map map, final Settlement settlement, final Terrain terraintype) {
         return map.getHexagon(mapSpot) != null &&
-                map.getHexagon(mapSpot).getTerrainType() == terraintype;
+                ((map.getHexagon(mapSpot).getTerrainType() == terraintype && map.getHexagon(mapSpot).isEmpty()) || settlement.isMapSpotInSettlement(mapSpot));
 
     }
 }
